203b25a10ea698095a4c42bd680ac98599bd1773
Kasia Brzezinska <kasiaplaskota4@gmail.com>
2019-09-30 14:22:47 +0200

    cleanscript.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,1 @@
-let computerMove = `kamień`;
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-
-let playerMove = 'papier';
-
-printMessage('Zagrałeś ' + playerMove + '! Wygrywasz!');
